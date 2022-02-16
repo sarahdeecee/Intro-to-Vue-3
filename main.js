7207abbad120af636d881850ea245bbe8ce71da0
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-02-15 23:35:07 -0500

    add link ro image and image alt text

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'Just some socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'file:///Users/sdelacruz/Documents/vue-intro/assets/images/socks_blue.jpg'
         }
     }
 })
